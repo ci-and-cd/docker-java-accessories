b4eacc80f135402642ea260ac5a81de066e02429
haolun <haolun@example.com>
2018-07-05 20:06:29 +0800

    push images

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker-compose version
 
 WORK_DIR=$(pwd)
 
-#if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
+if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
 export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
 export IMAGE_NAME=${IMAGE_NAME:-java-accessories}
@@ -15,9 +15,9 @@ export IMAGE_TAG=${IMAGE_ARG_NODE_VERSION:-latest}
 if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
 
 # Build image
-#if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
     docker-compose build image
-#fi
+fi
 
 # Build dumper image
 docker save ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} > dumper/docker/image.tar
@@ -26,5 +26,5 @@ docker-compose build dumper
 # Build archive image
 docker-compose build archive
 
-#docker-compose push image
-#docker-compose push archive
+docker-compose push image
+docker-compose push archive
