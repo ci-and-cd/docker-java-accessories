c61c489e0692d2815a08b491f1c929d5ed8bbabc
haolun zhang <opensource@infra.top>
2019-03-30 02:51:21 +0800

    update java_debug_monitor_profiler.sh

--- image/docker/java_debug_monitor_profiler.sh
+++ image/docker/java_debug_monitor_profiler.sh
@@ -4,16 +4,17 @@
 if [[ ! -z "${EUREKA_INSTANCE_HOSTNAME}" ]]; then
     JAVA_RMI_SERVER_HOSTNAME="${EUREKA_INSTANCE_HOSTNAME}"
 else
-    JAVA_RMI_SERVER_HOSTNAME="$(ip add show eth0 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')"
+    ip_addr=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
+    JAVA_RMI_SERVER_HOSTNAME="${ip_addr}"
 fi
-(>&2 echo "java.rmi.server.hostname:${JAVA_RMI_SERVER_HOSTNAME}")
+(>&2 echo "java.rmi.server.hostname: ${JAVA_RMI_SERVER_HOSTNAME}")
 
 if [[ ! -z "${JAVA_DEBUG_PORT}" ]]; then
     # For running remote JVM
     #JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT} ${JAVA_OPTS}";
     # For JDK 1.4.x +
     JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT} ${JAVA_OPTS}";
-    (>&2 echo "Java remote debug enabled, at ${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_DEBUG_PORT}")
+    (>&2 echo "Java remote debug enabled, at '${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_DEBUG_PORT}'")
 fi
 
 # see: https://www.jamasoftware.com/blog/monitoring-java-applications/
@@ -22,7 +23,7 @@ fi
 if [[ ! -z "${JAVA_JMX_PORT}" ]]; then
     if [[ "${JAVA_JMC_ENABLED}" = "true" ]]; then
         JAVA_OPTS="${JAVA_OPTS} -XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
-        (>&2 echo "Java JMC enabled, at ${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JMX_PORT}")
+        (>&2 echo "Java JMC enabled, at '${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JMX_PORT}'")
     fi
     JAVA_OPTS="${JAVA_OPTS} -Djava.rmi.server.hostname=${JAVA_RMI_SERVER_HOSTNAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.rmi.port=${JAVA_JMX_PORT}"
@@ -42,7 +43,7 @@ if [[ ! -z "${JAVA_JMX_PORT}" ]]; then
     else
         JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false"
     fi
-    (>&2 echo "Java JMX management enabled, at ${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JMX_PORT}")
+    (>&2 echo "Java JMX management enabled, at '${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JMX_PORT}'")
 fi
 
 # ejstatd, see: https://github.com/anthony-o/ejstatd
@@ -53,14 +54,14 @@ if [[ ! -z "${JAVA_JSTATD_RMI_PORT}" ]] && [[ ! -z "${JAVA_JSTATD_RH_PORT}" ]] &
         -pr${JAVA_JSTATD_RMI_PORT} \
         -ph${JAVA_JSTATD_RH_PORT} \
         -pv${JAVA_JSTATD_RV_PORT} &
-    (>&2 echo "Java jstatd enabled, at ${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JSTATD_RMI_PORT} ${JAVA_JSTATD_RH_PORT} ${JAVA_JSTATD_RV_PORT}")
+    (>&2 echo "Java jstatd enabled, at '${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JSTATD_RMI_PORT} ${JAVA_JSTATD_RH_PORT} ${JAVA_JSTATD_RV_PORT}'")
 fi
 
 # JProfiler agent, see: http://resources.ej-technologies.com/jprofiler/help/doc/sessions/remoteTable.html
 # find config.xml at client side ~/.jprofiler10/config.xml
 if [[ ! -z "${JAVA_JPROFILER_PORT}" ]] && [[ ! -z "${JAVA_JPROFILER_CONFIG}" ]]; then
     JAVA_OPTS="-agentpath:/opt/jprofiler/bin/linux-x64/libjprofilerti.so=port=${JAVA_JPROFILER_PORT},nowait,config=${JAVA_JPROFILER_CONFIG} ${JAVA_OPTS}"
-    (>&2 echo "Java JProfiler enabled, at ${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JPROFILER_PORT}")
+    (>&2 echo "Java JProfiler enabled, at '${JAVA_RMI_SERVER_HOSTNAME}:${JAVA_JPROFILER_PORT}'")
 fi
 
 # YourKit doc, see: https://www.yourkit.com/docs/
